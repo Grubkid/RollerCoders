595e19ec04a328abaf704b5a5ef7d725b009eff1
Grubkid <mylesloubon@gmail.com>
2025-04-24 02:07:21 -0400

    config

--- App/config.py
+++ App/config.py
@@ -1,11 +1,26 @@
 import os
 
 def load_config(app, overrides):
+    # Load environment-specific configuration
     if os.path.exists(os.path.join('./App', 'custom_config.py')):
         app.config.from_object('App.custom_config')
     else:
         app.config.from_object('App.default_config')
-    app.config.from_prefixed_env()
+
+    # Load additional secrets and API keys
+    app.config['ENV'] = os.environ.get('ENV', 'DEVELOPMENT')
+    if app.config['ENV'] == "DEVELOPMENT":
+        from .default_config import JWT_ACCESS_TOKEN_EXPIRES, SQLALCHEMY_DATABASE_URI, SECRET_KEY
+        app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
+        app.config['SECRET_KEY'] = SECRET_KEY
+        app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
+    else:
+        # For production, ensure these values are set via environment variables
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+        app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 7))
+
+    # Additional Flask configurations
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['PREFERRED_URL_SCHEME'] = 'https'
@@ -15,5 +30,7 @@ def load_config(app, overrides):
     app.config["JWT_COOKIE_SECURE"] = True
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
+
+    # Apply overrides if provided
     for key in overrides:
         app.config[key] = overrides[key]
