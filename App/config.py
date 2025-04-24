483ed2ddde9429c30dc2f2cbb14b5fe89f59d003
Grubkid <mylesloubon@gmail.com>
2025-04-24 00:13:31 -0400

    file

--- App/config.py
+++ App/config.py
@@ -1,16 +1,11 @@
 import os
-from .default_config import SQLALCHEMY_DATABASE_URI, SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES
 
 def load_config(app, overrides):
     if os.path.exists(os.path.join('./App', 'custom_config.py')):
         app.config.from_object('App.custom_config')
     else:
         app.config.from_object('App.default_config')
-    
-    # Load environment variables with a prefix
     app.config.from_prefixed_env()
-    
-    # Default configurations
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['PREFERRED_URL_SCHEME'] = 'https'
@@ -20,21 +15,5 @@ def load_config(app, overrides):
     app.config["JWT_COOKIE_SECURE"] = True
     app.config["JWT_COOKIE_CSRF_PROTECT"] = False
     app.config['FLASK_ADMIN_SWATCH'] = 'darkly'
-
-    # Handle environment-specific configurations
-    app.config['ENV'] = os.environ.get('ENV', 'DEVELOPMENT')
-    if app.config['ENV'] == "DEVELOPMENT":
-        # Use default_config for development
-        from .default_config import SQLALCHEMY_DATABASE_URI, SECRET_KEY, JWT_ACCESS_TOKEN_EXPIRES
-        app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-        app.config['SECRET_KEY'] = SECRET_KEY
-        app.config['JWT_ACCESS_TOKEN_EXPIRES'] = JWT_ACCESS_TOKEN_EXPIRES
-    else:
-        # Use environment variables for production
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
-        app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-        app.config['JWT_ACCESS_TOKEN_EXPIRES'] = int(os.environ.get('JWT_ACCESS_TOKEN_EXPIRES', 7))
-
-    # Apply overrides
     for key in overrides:
         app.config[key] = overrides[key]
